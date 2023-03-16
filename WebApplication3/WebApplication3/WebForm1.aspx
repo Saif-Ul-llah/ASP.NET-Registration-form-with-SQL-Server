af3dc7dc5c527718a21dfd7b47f4a80c8b1b851a
Saif-Ul-llah <saifhammad411@gmail.com>
2023-03-17 03:12:30 +0500

    NEW CODE

---
+++ WebApplication3/WebApplication3/WebForm1.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Registration Form</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <style>
+        body {
+    font-family: Arial, sans-serif;
+}
+
+form {
+    width: 50%;
+    margin: 0 auto;
+}
+
+label {
+    display: inline-block;
+    width: 150px;
+    margin-bottom: 10px;
+}
+
+input[type="text"], input[type="password"], input[type="file"] {
+    display: block;
+    width: 100%;
+    padding: 5px;
+    margin-bottom: 20px;
+}
+
+button[type="submit"] {
+    background-color: #4CAF50;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+button[type="submit"]:hover {
+    background-color: #45a049;
+}
+
+.error {
+    color: red;
+    font-size: 12px;
+    margin-top: 5px;
+}
+
+    </style>
+</head>
+<body>
+    <form id="RegistrationForm" runat="server">
+        <div>
+            <h1>Registration Form</h1>
+            <hr />
+
+            <label for="FirstNameTextBox">First Name:</label>
+            <asp:TextBox ID="FirstNameTextBox" runat="server" Required="true"></asp:TextBox>
+
+            <label for="LastNameTextBox">Last Name:</label>
+            <asp:TextBox ID="LastNameTextBox" runat="server" Required="true"></asp:TextBox>
+
+            <label for="EmailTextBox">Email:</label>
+            <asp:TextBox ID="EmailTextBox" runat="server" Required="true"></asp:TextBox>
+
+            <label for="PasswordTextBox">Password:</label>
+            <asp:TextBox ID="PasswordTextBox" runat="server" TextMode="Password" Required="true"></asp:TextBox>
+
+            <label for="FileUploadControl">File Upload:</label>
+            <asp:FileUpload ID="FileUploadControl" runat="server" />
+
+            <asp:Button ID="SubmitButton" runat="server" Text="Submit" OnClick="SubmitButton_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            string firstName = FirstNameTextBox.Text;
+            string lastName = LastNameTextBox.Text;
+            string email = EmailTextBox.Text;
+            string password = PasswordTextBox.Text;
+            string fileName = "";
+
+            // Check if a file was uploaded and save it to the server
+            if (FileUploadControl.HasFile)
+            {
+                fileName = FileUploadControl.FileName;
+                string path = Server.MapPath("~/uploads/") + fileName;
+                FileUploadControl.SaveAs(path);
+            }
+
+            // Insert the data into the database
+            string connectionString = "Data Source=DESKTOP-EFVSMR6;Initial Catalog=SAIF;Integrated Security=True";
+            string query = "INSERT INTO Users (FirstName, LastName, Email, Password, FileName) VALUES (@FirstName, @LastName, @Email, @Password, @FileName)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@FirstName", firstName);
+                command.Parameters.AddWithValue("@LastName", lastName);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Password", password);
+                command.Parameters.AddWithValue("@FileName", fileName);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            // Redirect to a success page
+            Response.Redirect("WebForm1.aspx");
+        }
+    }
+
+}
+    
